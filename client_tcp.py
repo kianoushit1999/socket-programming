11feadfd04c7f5fe7f954e80b544714cc9c5f57f
kianoush <kianoushit1999@gmail.com>
2021-10-24 13:34:10 -0700

    compelete both, serverside and client side

--- client_tcp.py
+++ client_tcp.py
@@ -7,14 +7,15 @@ if __name__ == '__main__':
     with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as client:
         client.connect((HOST, PORT))
 
-        file_name = input("Enter your content: \n")
-        saved_addr = input("Enter your address(for save): \n")
-
-        client.sendall(file_name.encode())
         while(True):
+            file_name = input("Enter your content: \n")
+            saved_addr = input("Enter your address(for save): \n")
+            client.sendall(file_name.encode())
             data = client.recv(2048)
-            if ("ok" not in data.decode()):
+            if ("OK" not in data.decode()):
                 print("404, Not found")
-            html_content = client.recv(4096)
+                break
+            print("200, Was found")
+            html_content = client.recv(1048576)
             with open(saved_addr, 'w') as writer:
-                writer.write(html_content.decode())
+                writer.writelines(html_content.decode())
--- server_tcp.py
+++ server_tcp.py
@@ -1,17 +1,27 @@
 import socket
+import os
 
 if __name__ == '__main__':
 
     HOST = 'localhost'
     PORT = 65432
-
+    os.chdir('serverfile')
     with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
         with conn:
             while True:
-                data = conn.recv(1024)
-                if data == 'end':
+                filename = conn.recv(1024).decode()
+                if (filename in os.listdir()):
+                    msg = "OK, 200"
+                    conn.sendall(msg.encode())
+                    html_content = ''
+                    with open(filename, 'r') as reader:
+                        for line in reader.readlines():
+                            html_content += line
+                    conn.sendall(html_content.encode())
+                else:
+                    msg = "404, Not Found"
+                    conn.sendall(msg.encode())
                     break
-                conn.sendall(data.decode().capitalize().encode())
